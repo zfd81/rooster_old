761762a7fdec81324b92591657ef070ee001f78d
zfd81 <zfd81@189.cn>
2019-03-19 22:06:32 +0800

    修改bindMap方法

--- xsql/sqlParser.go
+++ xsql/sqlParser.go
@@ -6,15 +6,46 @@ import (
 	"github.com/zfd81/rooster/util"
 )
 
+// func bindMap(sql string, arg map[string]interface{}) (string, []interface{}, error) {
+// 	arglist := make([]interface{}, 0, 20)
+// 	newSql, err := util.ReplaceByKeyword(sql, ':', func(index int, start int, end int, content string) (string, error) {
+// 		val, ok := arg[content]
+// 		if !ok {
+// 			return "?", fmt.Errorf("could not find name %s in %#v", content, arg)
+// 		}
+// 		arglist = append(arglist, val)
+// 		return "?", nil
+// 	})
+// 	return newSql, arglist, err
+// }
+
 func bindMap(sql string, arg map[string]interface{}) (string, []interface{}, error) {
-	arglist := make([]interface{}, 0, 20)
-	newSql, err := util.ReplaceByKeyword(sql, ':', func(index int, start int, end int, content string) (string, error) {
+	newSql, err := util.ReplaceBetween(sql, "{", "}", func(index int, start int, end int, content string) (string, error) {
+		ignore := false
+		fragment, err := util.ReplaceByKeyword(content, ':', func(i int, s int, e int, c string) (string, error) {
+			_, ok := arg[c]
+			if !ok {
+				ignore = true
+				return "", nil
+			}
+			return fmt.Sprintf(":%s", c), nil
+		})
+		if ignore {
+			return "", err
+		}
+		return fragment, err
+	})
+	if err != nil {
+		return "", nil, err
+	}
+	params := make([]interface{}, 0, 20)
+	newSql, err = util.ReplaceByKeyword(newSql, ':', func(index int, start int, end int, content string) (string, error) {
 		val, ok := arg[content]
 		if !ok {
 			return "?", fmt.Errorf("could not find name %s in %#v", content, arg)
 		}
-		arglist = append(arglist, val)
+		params = append(params, val)
 		return "?", nil
 	})
-	return newSql, arglist, err
+	return newSql, params, err
 }
--- xsql/sqlParser_test.go
+++ xsql/sqlParser_test.go
@@ -4,10 +4,13 @@ import (
 	"testing"
 )
 
-func Test_variable(t *testing.T) {
-	str := "select name from tbale where name=:name and pwd =:pwd and age>1"
-	for i := 0; i < 10; i++ {
-		t.Log(variable(str))
-		// variable(str)
-	}
+func Test_bindMap(t *testing.T) {
+	str := "select name from tbale where name=:name {and pwd =:pwd} and{} {age>1}"
+	countryCapitalMap := make(map[string]interface{})
+	countryCapitalMap["name"] = "Paris"
+	countryCapitalMap["pwd"] = "Rome"
+	str, params, err := bindMap(str, countryCapitalMap)
+	t.Log(str)
+	t.Log(params)
+	t.Log(err)
 }
