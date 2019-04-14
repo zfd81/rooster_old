9e43e9849d0493750c4cc9e931e17808aaf0d917
zfd81 <zfd81@189.cn>
2019-04-14 10:17:34 +0800

    mac测试

--- util/stringUtils_test.go
+++ util/stringUtils_test.go
@@ -28,7 +28,7 @@ func TestIndexOf(t *testing.T) {
 
 func TestReplaceBetween(t *testing.T) {
 
-	str := "select name, age, sex from table where id=#id# and age>#aa#"
+	str := "select name, age, sex from table where id=#id# and age>#aaa#"
 	var newStr string
 	for i := 0; i < 1000000; i++ {
 		newStr, _ = ReplaceBetween(str, "#", "#", func(index int, start int, end int, content string) (string, error) {
