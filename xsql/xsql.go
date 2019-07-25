0f04b3a56bce0749957356ca49dc51ce3b46bd0e
zfd81 <zfd81@189.cn>
2019-07-25 14:09:09 +0800

    添加save和Exec方法

--- xsql/xsql.go
+++ xsql/xsql.go
@@ -6,8 +6,9 @@ import (
 
 type DB struct {
 	*sql.DB
-	driverName string
-	unsafe     bool
+	driverName     string
+	dataSourceName string
+	unsafe         bool
 }
 
 func Open(driverName, dataSourceName string) (*DB, error) {
@@ -15,7 +16,41 @@ func Open(driverName, dataSourceName string) (*DB, error) {
 	if err != nil {
 		return nil, err
 	}
-	return &DB{DB: db, driverName: driverName}, err
+	return &DB{DB: db, driverName: driverName, dataSourceName: dataSourceName}, err
+}
+
+func (db *DB) Save(table string, arg Params) (int64, error) {
+	sql, params, err := insert(table, arg)
+	if err != nil {
+		return -1, err
+	}
+	stmt, err := db.DB.Prepare(sql)
+	if err != nil {
+		return -1, err
+	}
+	res, err := stmt.Exec(params...)
+	if err != nil {
+		return -1, err
+	}
+	num, err := res.RowsAffected() //影响行数
+	return num, err
+}
+
+func (db *DB) Exec(query string, arg Params) (int64, error) {
+	sql, params, err := bindParams(query, arg)
+	if err != nil {
+		return -1, err
+	}
+	stmt, err := db.DB.Prepare(sql)
+	if err != nil {
+		return -1, err
+	}
+	res, err := stmt.Exec(params...)
+	if err != nil {
+		return -1, err
+	}
+	num, err := res.RowsAffected() //影响行数
+	return num, err
 }
 
 //func (db *DB) XExec(query string, param Paramer) (int64, error) {
